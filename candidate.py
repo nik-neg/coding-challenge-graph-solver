85c1be44f99ca34832d2bcff062cdbb0c520b0e1
nik-neg <50702358+nik-neg@users.noreply.github.com>
2019-06-06 21:19:56 +0200

    corrected writing

--- candidate.py
+++ candidate.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 @author: Alex
-         This porgramm was written on Anaconda with Spyder 3.2.6 (Python 3.6)
+         This programm was written on Anaconda with Spyder 3.2.6 (Python 3.6)
 """
 # Candidate class for the nodes,
 # which are in a candidates list
